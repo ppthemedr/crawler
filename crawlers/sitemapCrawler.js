980ef1742c4fee679622f58f324257059c559361
ppthemedr <32570172+ppthemedr@users.noreply.github.com>
2025-05-23 10:08:20 +0200

    Update sitemapCrawler.js

--- crawlers/sitemapCrawler.js
+++ crawlers/sitemapCrawler.js
@@ -1,14 +1,14 @@
-/* SIMPLE SITEMAP PARSER
+/* SITEMAP-CRAWLER
 --------------------------------------------------- */
 import { XMLParser } from 'fast-xml-parser';
 
-export async function sitemapCrawler(startUrl, runId) {
+export async function sitemapCrawler(startUrl, runId, options = {}) {
   const sitemapUrl = new URL('/sitemap.xml', startUrl).href;
-  const res   = await fetch(sitemapUrl);
+  const res = await fetch(sitemapUrl);
   if (!res.ok) throw new Error(`No sitemap.xml at ${sitemapUrl}`);
 
   const xml    = await res.text();
-  const parser = new XMLParser();
+  const parser = new XMLParser({ ignoreAttributes: false });
   const json   = parser.parse(xml);
   const urls   = json.urlset?.url?.map(u => u.loc) ?? [];
 
